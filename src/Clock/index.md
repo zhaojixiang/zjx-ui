19338c1bde702c864fa7c3590314dbb3efea4601
赵吉祥 <zhaojx@weipaitang.com>
2022-05-23 08:36:04 +0800

    first commmit

--- src/Clock/index.md
+++ src/Clock/index.md
@@ -14,24 +14,54 @@ Demo:
 import React from 'react';
 import { Clock } from 'zjx-ui';
 
-export default () => <Clock />;
+export default () => <Clock type="rect"/>;
 ```
 
-矩形填充 描边 擦除:
+### 矩形填充 描边 擦除:
+
 | 属性 | 描述 |
 | --- | --- |
 | fillStyle | 填充颜色 |
 | fillRect | 矩形填充 |
 | strokeStyle | 描边 |
 | strokeRect | 矩形描边 |
+| rect(x, y, width, height) | 绘制一个左上角坐标为（x,y），宽高为width以及height的矩形。 |
 
-路径:
+### 圆弧:
+
+```tsx
+import React from 'react';
+import { Clock } from 'zjx-ui';
+
+export default () => <Clock type="arc"/>;
+```
+
 | 属性 | 描述 |
 | --- | --- |
-| arc | 填充颜色 |
-| arcTo | 矩形填充 |
-| strokeStyle | 描边 |
-| strokeRect | 矩形描边 |
+| arc(x, y, radius, startAngle, endAngle, anticlockwise) | 画一个以（x,y）为圆心的以radius为半径的圆弧（圆），从startAngle开始到endAngle结束，按照anticlockwise给定的方向（默认为顺时针）来生成。 *注意: 弧度=(Math.PI/180)\*角度*|
+| arcTo(x1, y1, x2, y2, radius) | 根据给定的控制点和半径画一段圆弧，再以直线连接两个控制点。 |
 
+### 二次贝塞尔曲线及三次贝塞尔曲线:
+
+```tsx
+import React from 'react';
+import { Clock } from 'zjx-ui';
+
+export default () => <Clock type=""/>;
+```
+
+| 属性 | 描述 |
+| --- | --- |
+| quadraticCurveTo(cp1x, cp1y, x, y) | 绘制二次贝塞尔曲线，cp1x,cp1y为一个控制点，x,y为结束点。 |
+| bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y) | 绘制三次贝塞尔曲线，cp1x,cp1y为控制点一，cp2x,cp2y为控制点二，x,y为结束点。 |
+
+## 综合示例
+
+```tsx
+import React from 'react';
+import { Clock } from 'zjx-ui';
+
+export default () => <Clock type="normal"/>;
+```
 
 <!-- More skills for writing demo: https://d.umijs.org/guide/basic#write-component-demo -->
--- src/Clock/index.tsx
+++ src/Clock/index.tsx
@@ -1,13 +1,29 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
-export default () => {
+interface PropsType {
+  type: string; // 绘制类型
+  width: number | string; // 画布宽度
+  height: number | string; // 画布高度
 
+}
+export default (props: PropsType) => {
+  const { type, width = 300, height = 200 } = props
   useEffect(() => {
-    drawClock()
+    switch (type) {
+      case 'rect':
+        drawRect()
+        break;
+      case 'arc':
+        drawArc()
+        break;
+      case 'normal':
+        draw();
+        break;
+    }
   }, [])
-
-  const drawClock = () => {
-    let draw: any = document.getElementById('drawing');
+  // 矩形
+  const drawRect = () => {
+    let draw: any = document.getElementById('rect');
     if (draw.getContext) {
       const ctx = draw.getContext("2d");
       // 矩形
@@ -25,23 +41,107 @@ export default () => {
 
       ctx.strokeStyle = "pink";
       ctx.strokeRect(180, 30, 50, 50);
-
-      // 路径
+    }
+  }
+  // 圆弧
+  const drawArc = () => {
+    let draw: any = document.getElementById('arc');
+    if (draw.getContext) {
+      const ctx = draw.getContext("2d");
       ctx.beginPath();
       ctx.strokeStyle = "gold";
-      ctx.arc(200, 200, 60, 0, 2 * Math.PI, false);
-      ctx.moveTo(255, 200);
-      ctx.arc(200, 200, 55, 0, 10, false);
-      ctx.moveTo(200, 200);
-      ctx.lineTo(240, 200);
-      ctx.moveTo(200, 200);
-      ctx.lineTo(200, 150);
+      ctx.arc(100, 100, 60, 0, 2 * Math.PI, false);
+      ctx.moveTo(155, 100);
+      ctx.arc(100, 100, 55, 0, 10, false);
+      ctx.moveTo(100, 100);
+      ctx.lineTo(140, 100);
+      ctx.moveTo(100, 100);
+      ctx.lineTo(100, 50);
       ctx.stroke();
+    }
+  }
+  const roundedRect = (ctx: any, x: number, y: number, width: number, height: number, radius: number) => {
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.lineTo(x, y + height - radius);
+    ctx.quadraticCurveTo(x, y + height, x + radius, y + height);
+    ctx.lineTo(x + width - radius, y + height);
+    ctx.quadraticCurveTo(x + width, y + height, x + width, y + height - radius);
+    ctx.lineTo(x + width, y + radius);
+    ctx.quadraticCurveTo(x + width, y, x + width - radius, y);
+    ctx.lineTo(x + radius, y);
+    ctx.quadraticCurveTo(x, y, x, y + radius);
+    ctx.stroke();
+  }
+  const draw = () => {
+    let canvas: any = document.getElementById('normal');
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
 
+      roundedRect(ctx, 12, 12, 150, 150, 15);
+      roundedRect(ctx, 19, 19, 150, 150, 9);
+      roundedRect(ctx, 53, 53, 49, 33, 10);
+      roundedRect(ctx, 53, 119, 49, 16, 6);
+      roundedRect(ctx, 135, 53, 49, 33, 10);
+      roundedRect(ctx, 135, 119, 25, 49, 10);
+
+      ctx.beginPath();
+      ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
+      ctx.lineTo(31, 37);
+      ctx.fill();
+
+      for (var i = 0; i < 8; i++) {
+        ctx.fillRect(51 + i * 16, 35, 4, 4);
+      }
+
+      for (i = 0; i < 6; i++) {
+        ctx.fillRect(115, 51 + i * 16, 4, 4);
+      }
+
+      for (i = 0; i < 8; i++) {
+        ctx.fillRect(51 + i * 16, 99, 4, 4);
+      }
+
+      ctx.beginPath();
+      ctx.moveTo(83, 116);
+      ctx.lineTo(83, 102);
+      ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+      ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+      ctx.lineTo(111, 116);
+      ctx.lineTo(106.333, 111.333);
+      ctx.lineTo(101.666, 116);
+      ctx.lineTo(97, 111.333);
+      ctx.lineTo(92.333, 116);
+      ctx.lineTo(87.666, 111.333);
+      ctx.lineTo(83, 116);
+      ctx.fill();
+
+      ctx.fillStyle = "white";
+      ctx.beginPath();
+      ctx.moveTo(91, 96);
+      ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+      ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+      ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+      ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+      ctx.moveTo(103, 96);
+      ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+      ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+      ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+      ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+      ctx.fill();
+
+      ctx.fillStyle = "black";
+      ctx.beginPath();
+      ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+      ctx.fill();
     }
   }
 
   return <div>
-    <canvas id='drawing' width={400} height={400}/>
+    <canvas id={type} width={width} height={height} />
   </div>
 };
